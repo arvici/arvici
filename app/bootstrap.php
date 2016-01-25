d67166944684c5a7f6f10a1deeba0458ae1da7d9
Tom Valk <tomvalk@gmail.com>
2016-01-25 19:16:04 +0100

    Update app bootstrap file.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,19 +1,37 @@
 <?php
 /**
- * Arvici App Bootstrap
+ * Arvici App Bootstrap.
  *
  * @author     Tom Valk <tomvalk@lt-box.info>
  * @copyright  2016 Tom Valk
  */
 
-defined('BASEPATH') || define('BASEPATH', __DIR__ . '/../');
-defined('APPPATH') || define('APPPATH', __DIR__ . '/');
-$configDir = APPPATH . 'Config/';
+/**
+ * Define the base directory containing the 'App' folder.
+ */
+defined('BASEPATH') || define('BASEPATH', __DIR__ . DS . '..' . DS);
 
-require_once $configDir . 'App.php';
-require_once $configDir . 'Router.php';
-require_once $configDir . 'Template.php';
-require_once $configDir . 'Database.php';
+/**
+ * Define all the paths in the base.
+ */
+defined('APPPATH') || define('APPPATH', BASEPATH . 'App' . DS);
 
-// Run the router.
+
+
+/**
+ * Load all configuration files.
+ */
+$configDir = APPPATH . '/Config/';
+foreach (glob($configDir . '*.php') as $fileName) {
+    require_once $fileName;
+}
+
+/**
+ * Start the logger.
+ */
+// TODO: Implement logger here.
+
+/**
+ * Start the router.
+ */
 \Arvici\Component\Router::getInstance()->run();
