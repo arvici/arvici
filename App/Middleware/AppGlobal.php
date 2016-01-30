4517decc291f6d8a84576c160089bb4e79807b6e
Tom Valk <tomvalk@gmail.com>
2016-01-30 15:21:59 +0100

    Adding sample middleware.

---
+++ App/Middleware/AppGlobal.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * AppGlobal.php
+ *
+ * @author     Tom Valk <tomvalk@lt-box.info>
+ * @copyright  2016 Tom Valk
+ */
+
+namespace App\Middleware;
+
+
+use Arvici\Component\Middleware\BaseMiddleware;
+
+class AppGlobal extends BaseMiddleware
+{
+    public function before()
+    {
+        \Logger::debug("Starting controllers, before middleware called");
+    }
+
+    public function after()
+    {
+        \Logger::debug("Stopping controllers, after middleware called");
+    }
+}
